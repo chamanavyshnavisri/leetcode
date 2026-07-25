c8a0c0463103bdf88673ed4c84fe2cc50d349c6e
chamanavyshnavisri <chamanavyshnavisri@gmail.com>
2026-07-25 10:32:00 +0530

    Time: 7 ms (40.27%), Space: 20.5 MB (23.74%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,21 +1,15 @@
-class Solution(object):
-    def moveZeroes(self, nums):
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
         n=len(nums)
-        index=-1
+        j=0
         for i in range(n):
-            if nums[i]==0:
-                index=i
-                break
-        if index==-1:
-            return
-        for j in range(index+1,n):
-            if nums[j]!=0:
-                nums[index],nums[j]=nums[j],nums[index]
-                index+=1
+            if nums[i]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                j+=1
+        
 
-
-                    
+        
+        
